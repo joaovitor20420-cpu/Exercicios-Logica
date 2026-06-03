60b2ad334b3761c2538f5d380588ffb41da15a49
Joao Vitor <joaovitor20420@gmail.com>
2026-06-03 20:48:43 -0300

    feat: initialize project with Java syntax exercise and IDE configuration files

---
+++ Fase 1/Sintaxe.java
@@ -0,0 +1,21 @@
+public class Sintaxe {
+    public static void main(String[] args) {
+        int numero = 2;
+        System.out.println(numero);
+
+        String nome = "Joao";
+        System.out.println(nome);
+
+        double valor = 20.39;
+        System.out.println(valor);
+
+        float numero2 = 2.98f;
+        System.out.println(numero2);
+
+        boolean status = false;
+        System.out.println(status);
+
+        char letra = 'J';
+        System.out.println(letra);
+    }
+}
