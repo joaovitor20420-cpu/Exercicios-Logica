32f7cdcec9e69e7d3a69abcd74ff72199843d895
Joao Vitor <joaovitor20420@gmail.com>
2026-09-08 22:31:37 -0300

    feat:add InverterArrey reverse array exercise

---
+++ Fase 2/Arrey/Arrey/InverterArrey.java
@@ -0,0 +1,18 @@
+package Arrey;
+
+public class InverterArrey {
+    public static void main(String[] args){
+        int numero[] = {1, 2, 3, 4, 5};
+        int temp = numero[0];
+        numero[0] = numero[4];
+        numero[4] = temp;
+
+        int temp2 = numero[1];
+        numero[1] = numero[3];
+        numero[3] = temp2;
+
+        for(int i = 0;i < numero.length; i++){
+            System.out.println(numero[i]);
+        }
+    }
+}
