a7918a7bcdc941bcf1e20b435ac8f2ce2d0e9ec7
Joao Vitor <joaovitor20420@gmail.com>
2026-09-16 00:05:53 -0300

    feat(methods): add main menu for ValidarSenha exercise

--- Fase 2/metodos/ValidarSenha.java
+++ Fase 2/metodos/ValidarSenha.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ValidarSenha {
 
     public static boolean validarSenha(String senha) {
@@ -12,8 +14,21 @@ public class ValidarSenha {
             if (letra >= '0' && letra <= '9') {
                 TemNumero = true;
             }
-            
+
         }
         return TemNumero;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Digite sua senha: ");
+            String senha = scanner.next();
+
+            if (validarSenha(senha)) {
+                System.out.println("Sua senha é forte!!");
+            } else {
+                System.out.println("Sua senha é fraca. ");
+            }
+        }
     }
 }
