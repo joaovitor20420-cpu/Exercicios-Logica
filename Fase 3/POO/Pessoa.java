ed76b1088ee5bb15d5f7ef8a6b80153f77f88351
Joao Vitor <joaovitor20420@gmail.com>
2026-09-16 22:58:22 -0300

    feat(poo): add Pessoa class and first object instantiation

---
+++ Fase 3/POO/Pessoa.java
@@ -0,0 +1,10 @@
+package POO;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public void apresentar(){
+         System.out.println("OLa meu nome é " + nome + " e tenho " + idade + " anos!!");
+    }
+}
---
+++ Fase 3/POO/ProgramaPrincipal.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class ProgramaPrincipal {public static void main(String[] args){
+    Pessoa pessoa = new Pessoa();
+
+    pessoa.nome = "João";
+    pessoa.idade = 22;
+
+    pessoa.apresentar();
+
+}
+}
